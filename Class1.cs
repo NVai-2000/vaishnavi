849b84c7b011db1773bd7d15d166cc7c3b6e54bc
NVai-2000 <dxc110dn01@datascience.manipal.edu>
2021-06-02 15:12:49 +0530

    commited

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Class1
+    {
+        public void Discountcalculation()
+        {
+            int price = Convert.ToInt32(Console.ReadLine());
+            int discount = Convert.ToInt32(Console.ReadLine());
+            int calculate = Convert.ToInt32((price * discount) / 100);
+            Console.WriteLine("The discount for given Price is " + calculate);
+        }
+
+    }
+}
