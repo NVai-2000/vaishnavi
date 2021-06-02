3c8740a5bdcc6705e34dcddd766e5dd1948fcc80
NVai-2000 <85218977+NVai-2000@users.noreply.github.com>
2021-06-02 14:33:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculator;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = Class1();
+            obj = Discountcalculation();
+            Console.WriteLine("Discount is calculated");
+            Console.ReadLine();
+        }
+    }
+}
